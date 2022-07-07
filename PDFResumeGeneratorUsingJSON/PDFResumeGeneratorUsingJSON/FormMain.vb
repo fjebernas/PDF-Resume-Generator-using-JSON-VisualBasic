6f09d2f7cae5c68ab4637a39ca11fac02909def2
francisjosephironman <francis.3.6.joseph@gmail.com>
2022-07-07 10:50:05 +0800

    Added necessary NuGet packages.

--- PDFResumeGeneratorUsingJSON/PDFResumeGeneratorUsingJSON/FormMain.vb
+++ PDFResumeGeneratorUsingJSON/PDFResumeGeneratorUsingJSON/FormMain.vb
@@ -1,4 +1,10 @@
 ﻿Public Class FormMain
+    Private Const PATH_JSON As String = "C:\Users\franc\Documents\School\ELECTIVE 3\repos\PDFResumeGeneratorUsingJSON-folder\PDFResumeGeneratorUsingJSON\json-files"
+
+    Private namePDF_JSON As String
+    Private fullName, contactNo, address, email, objective, college, collegeDetail1, collegeDetail2, highschool, elementary, skill1, skill2, skill3, skill4, notes As String
+    Private isReadingJson As Boolean
+
     Public Sub New()
 
         ' This call is required by the designer.
@@ -6,5 +12,22 @@
 
         ' Add any initialization after the InitializeComponent() call.
 
+        For Each txtBx As TextBox In panelFields.Controls.OfType(Of TextBox)
+            txtBx.Text = ""
+            txtBx.ReadOnly = True
+        Next
+
+        btnSaveToJSON.Visible = False
+
+        Try
+
+
+        Catch ex As Exception
+
+        End Try
     End Sub
+
+    Private Function SplitName()
+
+    End Function
 End Class
